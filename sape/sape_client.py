a00ccc35b6717cf38a54b24a1e12df2a53e33fb6
Mikhail Fedosov <tbs.micle@gmail.com>
2013-05-30 14:02:55 +0400

    Создание директории для кеша, если таковой не существует

--- sape/sape_client.py
+++ sape/sape_client.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 
 import os
 import pickle
@@ -12,113 +13,120 @@ CACHE_LIFETIME = 3600
 
 
 class SapeFetchError(Exception):
-    pass
+	pass
 
 
 class SapeClient(object):
-    cache = None
-    cache_time = None
+	cache = None
+	cache_time = None
 
-    def __init__(self):
-        self.init_log()
-        self.get_links_from_cache()
+	def __init__(self):
+		self.init_log()
+		self.get_links_from_cache()
 
-    def get_links_from_cache(self):
-        filename = self.get_cache_file()
-        self.cache = {}
+	def get_links_from_cache(self):
+		filename = self.get_cache_file()
+		self.cache = {}
 
-        try:
-            self.log.info('Load links from file "%s"', filename)
-            self.log.info('Cache time %s, file time %s, id %s', self.cache_time, os.path.getmtime(filename), id(self))
-            data = open(filename, 'rb').read()
-            cache = pickle.loads(data)
-            self.cache = cache
-            self.cache_time = os.path.getmtime(filename)
+		try:
+			self.log.info('Load links from file "%s"', filename)
+			self.log.info('Cache time %s, file time %s, id %s', self.cache_time, os.path.getmtime(filename), id(self))
+			data = open(filename, 'rb').read()
+			cache = pickle.loads(data)
+			self.cache = cache
+			self.cache_time = os.path.getmtime(filename)
 
-        except (EOFError, IOError, OSError):
-            self.log.debug('Cache file "%s" not found', filename)
+		except (EOFError, IOError, OSError):
+			self.log.debug('Cache file "%s" not found', filename)
 
-    def init_log(self):
-        self.log = logging.getLogger('sape')
-        self.log.setLevel(getattr(settings, 'LOG_LEVEL', logging.ERROR))
-        handler = logging.FileHandler(getattr(settings, 'SAPE_LOG', '/dev/null'))
-        LOG_FORMAT = u'%(levelname)s %(asctime)s: %(message)s'
-        LOG_TIME_FORMAT = u'%Y-%m-%d %H:%M:%S'
-        handler.setFormatter(logging.Formatter(LOG_FORMAT, LOG_TIME_FORMAT))
-        self.log.addHandler(handler)
+	def init_log(self):
+		self.log = logging.getLogger('sape')
+		self.log.setLevel(getattr(settings, 'LOG_LEVEL', logging.ERROR))
+		handler = logging.FileHandler(getattr(settings, 'SAPE_LOG', '/dev/null'))
+		LOG_FORMAT = u'%(levelname)s %(asctime)s: %(message)s'
+		LOG_TIME_FORMAT = u'%Y-%m-%d %H:%M:%S'
+		handler.setFormatter(logging.Formatter(LOG_FORMAT, LOG_TIME_FORMAT))
+		self.log.addHandler(handler)
 
-    def get_links(self, uri):
-        if self.need_to_refresh():
-            self.get_links_from_cache()
+	def get_links(self, uri):
+		if self.need_to_refresh():
+			self.get_links_from_cache()
 
-        if uri in self.cache:
-            return self.cache[uri]
-        else:
-            return self.cache.get('__sape_new_url__', [])
+		if uri in self.cache:
+			return self.cache[uri]
+		else:
+			return self.cache.get('__sape_new_url__', [])
 
-    def need_to_refresh(self):
-        filename = self.get_cache_file()
+	def need_to_refresh(self):
+		filename = self.get_cache_file()
 
-        if not os.path.exists(filename):
-            self.log.error('Cache file "%s" not found', filename)
-            return False
+		if not os.path.exists(filename):
+			self.log.error('Cache file "%s" not found', filename)
+			return False
 
-        if not os.path.getsize(filename):
-            self.log.error('Cache file "%s" is empty', filename)
-            return False
+		if not os.path.getsize(filename):
+			self.log.error('Cache file "%s" is empty', filename)
+			return False
 
-        if os.path.getmtime(filename) > self.cache_time:
-            self.log.info('Fresh cache file "%s" found', filename)
-            return True
+		if os.path.getmtime(filename) > self.cache_time:
+			self.log.info('Fresh cache file "%s" found', filename)
+			return True
 
-        return False
+		return False
 
-    def update_cache(self):
-        self.log.info("Update cache for host %s: start", settings.SAPE_DOMAIN)
-        try:
-            links = self.fetch_links()
-            links_dict = self.parse_data(links)
-            self.save_links(links_dict)
-            self.log.info("Update cache for host %s: finish", settings.SAPE_DOMAIN)
-        except (IOError, UnicodeDecodeError), e:
-            message = "Cannot update cache file: %s (url %s)" % (e, self.sape_url())
-            self.log.error(message)
-            raise SapeFetchError(message)
+	def update_cache(self):
+		self.log.info("Update cache for host %s: start", settings.SAPE_DOMAIN)
+		try:
+			links = self.fetch_links()
+			links_dict = self.parse_data(links)
+			self.save_links(links_dict)
+			self.log.info("Update cache for host %s: finish", settings.SAPE_DOMAIN)
+		except (IOError, UnicodeDecodeError), e:
+			message = "Cannot update cache file: %s (url %s)" % (e, self.sape_url())
+			self.log.error(message)
+			raise SapeFetchError(message)
 
-    def fetch_links(self):
-        return urllib.urlopen(self.sape_url()).read()
+	def fetch_links(self):
+		return urllib.urlopen(self.sape_url()).read()
 
-    def sape_url(self):
-        return SAPE_URL % {'user_id': settings.SAPE_USER, 'host': settings.SAPE_DOMAIN, 'charset': settings.SAPE_CHARSET}
+	def sape_url(self):
+		return SAPE_URL % {'user_id': settings.SAPE_USER, 'host': settings.SAPE_DOMAIN, 'charset': settings.SAPE_CHARSET}
 
-    def parse_data(self, data):
-        """
-        Parse raw data retrieved from sape server.
-        """
-        db = {}
-        data = data.decode(settings.SAPE_CHARSET)
-        data = data.split('\n', 2)
+	def parse_data(self, data):
+		"""
+		Parse raw data retrieved from sape server.
+		"""
+		db = {}
+		data = data.decode(settings.SAPE_CHARSET)
+		data = data.split('\n', 2)
 
-        if data[0].startswith('FATAL ERROR'):
-            raise SapeFetchError(data[0])
+		if data[0].startswith('FATAL ERROR'):
+			raise SapeFetchError(data[0])
 
-        db['__sape_delimiter__'] = data[1]
+		db['__sape_delimiter__'] = data[1]
 
-        for block in data[2].split('\n'):
-            slices = block.split('||SAPE||')
-            db[slices[0]] = slices[1:]
+		for block in data[2].split('\n'):
+			slices = block.split('||SAPE||')
+			db[slices[0]] = slices[1:]
 
-        return db
+		return db
 
-    def save_links(self, links_dict):
-        data = pickle.dumps(links_dict)
+	def save_links(self, links_dict):
+		data = pickle.dumps(links_dict)
 
-        filename = self.get_cache_file()
-        open(filename, 'wb').write(data)
+		filename = self.get_cache_file()
 
-    def get_cache_file(self):
-        prefix = re.sub(r'(?i)[^-a-z0-9]', '_', settings.SAPE_DOMAIN)
-        return '%s/%s.links.db' % (settings.SAPE_DIR, prefix)
+		try:
+			os.makedirs(os.path.dirname(filename))
+		except OSError:
+			# директория для кеша уже существует
+			pass
+
+		open(filename, 'wb').write(data)
+
+	def get_cache_file(self):
+		prefix = re.sub(r'(?i)[^-a-z0-9]', '_', settings.SAPE_DOMAIN)
+		return '%s/%s.links.db' % (os.path.dirname(settings.SAPE_DIR), prefix)
 
 
 sape_manager = SapeClient()
