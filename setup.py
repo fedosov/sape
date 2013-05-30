73ba7afa47befb9cf6e16d20806d99dad572df6e
Mikhail Fedosov <tbs.micle@gmail.com>
2013-05-30 15:24:39 +0400

    Описание пакета в `setup.py`

--- setup.py
+++ setup.py
@@ -2,8 +2,20 @@ from distutils.core import setup
 
 setup(
     name='sape',
-    description='Django + sape.ru. Not for wide use.',
-    version='0.2',
+    description='Django + sape.ru.',
+    version='0.2.1',
     packages=['sape'],
     #install_requires=['mechanize']
+	classifiers=
+	[
+		'Development Status :: 4 - Beta',
+		'Environment :: Console',
+		'Environment :: Web Environment',
+		'Intended Audience :: Developers',
+		'License :: OSI Approved :: Python Software Foundation License',
+		'Operating System :: MacOS :: MacOS X',
+		'Operating System :: Microsoft :: Windows',
+		'Operating System :: POSIX',
+		'Programming Language :: Python',
+	],
 )
