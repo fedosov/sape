697c3ea3f5f3acbcd443cd2e8fb02dccff3f62dd
Mikhail Fedosov <tbs.micle@gmail.com>
2013-05-30 15:27:23 +0400

    Замена названия пакета в `setup.py` на `django-sape`

--- setup.py
+++ setup.py
@@ -1,21 +1,20 @@
 from distutils.core import setup
 
-setup(
-    name='sape',
-    description='Django + sape.ru.',
-    version='0.2.1',
-    packages=['sape'],
-    #install_requires=['mechanize']
-	classifiers=
-	[
-		'Development Status :: 4 - Beta',
-		'Environment :: Console',
-		'Environment :: Web Environment',
-		'Intended Audience :: Developers',
-		'License :: OSI Approved :: Python Software Foundation License',
-		'Operating System :: MacOS :: MacOS X',
-		'Operating System :: Microsoft :: Windows',
-		'Operating System :: POSIX',
-		'Programming Language :: Python',
-	],
+setup(name='django-sape',
+      description='Django + sape.ru.',
+      version='0.2.1',
+      packages=['sape'],
+      #install_requires=['mechanize']
+      classifiers=
+      [
+	      'Development Status :: 4 - Beta',
+	      'Environment :: Console',
+	      'Environment :: Web Environment',
+	      'Intended Audience :: Developers',
+	      'License :: OSI Approved :: Python Software Foundation License',
+	      'Operating System :: MacOS :: MacOS X',
+	      'Operating System :: Microsoft :: Windows',
+	      'Operating System :: POSIX',
+	      'Programming Language :: Python',
+      ],
 )
