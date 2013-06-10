6f81600297e26adad8c6aa926b9a68ecb1bd5a3a
Mikhail Fedosov <tbs.micle@gmail.com>
2013-06-10 17:19:34 +0400

    Исправлен `setup.py`

--- setup.py
+++ setup.py
@@ -1,26 +1,33 @@
 from distutils.core import setup
 
-setup(name='django-sape',
-      description='Django + sape.ru.',
-      version='0.2.3',
-      packages=['sape'],
-      author='Dimmma',
-      author_email='https://github.com/Dimmma',
-      maintainer='Mikhail Fedosov',
-      maintainer_email='tbs.micle@gmail.com',
-      url='http://github.com/fedosov/sape',
-      keywords=['Django', 'sape', 'sape.ru', ],
-      #install_requires=['mechanize']
-      classifiers=
-      [
-	      'Development Status :: 4 - Beta',
-	      'Environment :: Console',
-	      'Environment :: Web Environment',
-	      'Intended Audience :: Developers',
-	      'License :: OSI Approved :: Python Software Foundation License',
-	      'Operating System :: MacOS :: MacOS X',
-	      'Operating System :: Microsoft :: Windows',
-	      'Operating System :: POSIX',
-	      'Programming Language :: Python',
-      ],
+setup(
+	name='django-sape',
+	description='Django + sape.ru.',
+	version='0.2.4',
+	packages=\
+	[
+		'sape',
+		'sape.management',
+		'sape.management.commands',
+		'sape.templatetags',
+	],
+	author='Dimmma',
+	author_email='https://github.com/Dimmma',
+	maintainer='Mikhail Fedosov',
+	maintainer_email='tbs.micle@gmail.com',
+	url='http://github.com/fedosov/sape',
+	keywords=['Django', 'sape', 'sape.ru', ],
+	#install_requires=['mechanize']
+	classifiers=
+	[
+		'Development Status :: 4 - Beta',
+		'Environment :: Console',
+		'Environment :: Web Environment',
+		'Intended Audience :: Developers',
+		'License :: OSI Approved :: Python Software Foundation License',
+		'Operating System :: MacOS :: MacOS X',
+		'Operating System :: Microsoft :: Windows',
+		'Operating System :: POSIX',
+		'Programming Language :: Python',
+	],
 )
